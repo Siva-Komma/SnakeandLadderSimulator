ce335bc9ce88e629a5a5e83eb9d4d24254797820
Siva-Komma <kommasivajyothi22@gmail.com>
2023-02-13 16:20:26 +0530

    The Player then checks for a Option

--- SnakeandLadder/SinglePlayerGame.cs
+++ SnakeandLadder/SinglePlayerGame.cs
@@ -15,6 +15,28 @@ namespace SnakeandLadder
             Random random= new Random();
             int die = random.Next(1,7);
             Console.WriteLine("Random Number: " + die);
+
+            Random random1= new Random();
+            int Option = random1.Next(1,4);
+            
+            switch(Option)
+            {
+                case 1:
+                    Console.WriteLine("No Play");
+                    Position += 0;
+                    Console.WriteLine("After getting NoPlay Player Position: " + Position);
+                    break;
+                case 2:
+                    Console.WriteLine("Ladder");
+                    Position += die;
+                    Console.WriteLine("After getting ladder player Position: " + Position);
+                    break;
+                case 3:
+                    Console.WriteLine("Snake");
+                    Position -= die;
+                    Console.WriteLine("After getting snake Player Position: " + Position);
+                    break;
+            }
         }
     }
 }
