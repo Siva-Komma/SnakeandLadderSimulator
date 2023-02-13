6832caa7373386821144a6f9a636ac3107f62429
Siva-Komma <kommasivajyothi22@gmail.com>
2023-02-13 16:36:21 +0530

    Rolling the until Player reach 100th position

--- SnakeandLadder/SinglePlayerGame.cs
+++ SnakeandLadder/SinglePlayerGame.cs
@@ -13,29 +13,40 @@ namespace SnakeandLadder
             int Position = 0;
             Console.WriteLine("Player Position: " + Position);
             Random random= new Random();
-            int die = random.Next(1,7);
-            Console.WriteLine("Random Number: " + die);
-
-            Random random1= new Random();
-            int Option = random1.Next(1,4);
-            
-            switch(Option)
+            while (Position < 100 )
             {
-                case 1:
-                    Console.WriteLine("No Play");
-                    Position += 0;
-                    Console.WriteLine("After getting NoPlay Player Position: " + Position);
-                    break;
-                case 2:
-                    Console.WriteLine("Ladder");
-                    Position += die;
-                    Console.WriteLine("After getting ladder player Position: " + Position);
-                    break;
-                case 3:
-                    Console.WriteLine("Snake");
-                    Position -= die;
-                    Console.WriteLine("After getting snake Player Position: " + Position);
-                    break;
+                int die = random.Next(1, 7);
+                Console.WriteLine("Random Number: " + die);
+
+                Random random1 = new Random();
+                int Option = random1.Next(1, 4);
+
+                switch (Option)
+                {
+                    case 1:
+                        Console.WriteLine("No Play");
+                        Position += 0;
+                        Console.WriteLine("After getting NoPlay Player Position: " + Position);
+                        break;
+                    case 2:
+                        Console.WriteLine("Ladder");
+                        Position += die;
+                        Console.WriteLine("After getting ladder player Position: " + Position);
+                        break;
+                    case 3:
+                        Console.WriteLine("Snake");
+                        Position -= die;
+                        if (Position <= 0)
+                        {
+                            Position = 0;
+                            Console.WriteLine("After getting snake and Position less than zero then Player Position: " + Position);
+                        }
+                        else
+                        {
+                            Console.WriteLine("After getting snake Player Position: " + Position);
+                        }
+                        break;
+                }
             }
         }
     }
